815fea0d9b4c0798e6ec679ff0a43dcc3c688a04
guillou herve <guillou.herve@gmail.com>
2019-10-15 12:04:12 +0200

    debug - add display of image dir

--- program/image-classification-tflite-codereef/benchmark.h
+++ program/image-classification-tflite-codereef/benchmark.h
@@ -403,6 +403,7 @@ public:
   void load_images(const std::vector<std::string>& batch_images) override {
     int image_offset = 0;
     for (auto image_file : batch_images) {
+      std::cout << "Image loading: " << image_file << std::endl;
       _in_data->load(image_file);
       _in_converter->convert(_in_data.get(), _in_ptr + image_offset);
       image_offset += _in_data->size();
