c60f06113d5d7dddaf4c3888695421ccbf242fe3
Heré GUILLOU <guillou.herve@gmail.com>
2019-10-17 14:18:14 +0200

    Chore - remove delay between classifications

--- program/image-classification-tflite-codereef/classification.cpp
+++ program/image-classification-tflite-codereef/classification.cpp
@@ -136,7 +136,7 @@ int main(int argc, char* argv[]) {
           std::cout << "ERROR: " << error_message << std::endl; // information from length_error printed
           std::cout << "Waiting for new input"<< std::endl; 
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        //std::this_thread::sleep_for(std::chrono::milliseconds(0));
       }
         
     });
